2fc37b2548b4b9bf1b24d9e61b6641126ea23569
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-12-01 17:03:46 +0100

    added more demos and descriptions

---
+++ lectures/01/demos/03_demo.js
@@ -0,0 +1,18 @@
+
+
+
+/**
+ * Calculates the mean age of given numbers
+* @param {number[]} numbers - The numbers to calculate the average of  
+* @returns {number} The average of the given numbers
+ */
+const calculateAverage = (numbers) => {
+    // trigger autocomplete via "ALT + ^" 
+    // switch suggestions via "ALT + `" and "ALT + ß"
+    // or: use "generate this"
+    // remove commends if copilot is being confused
+
+
+
+
+};
